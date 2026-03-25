62cf651a3824d22e7f8c471cecd118d79f0d7244
Dave Alabado <92829860+Moggle-Khraum@users.noreply.github.com>
2026-03-25 20:23:04 +0800

    Update useApi.ts

--- artifacts/novel-reader/hooks/useApi.ts
+++ artifacts/novel-reader/hooks/useApi.ts
@@ -2,5 +2,5 @@ import { directFetchNovelMeta, directFetchChapter } from './useDirectScraper';
 
 export type { NovelMeta, ChapterData } from './useDirectScraper';
 
-export const fetchNovelMeta = directFetchMeta;
+export const fetchNovelMeta = directFetchNovelMeta;  // Fixed: was directFetchMeta
 export const fetchChapter = directFetchChapter;
